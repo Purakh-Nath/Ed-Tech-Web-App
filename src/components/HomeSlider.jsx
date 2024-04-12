c56d514b252ae917ff7d9ef109616031e855374d
Purakh-Nath <purakhnath45@gmail.com>
2024-04-12 19:26:20 +0530

    Image Optimization

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -4,24 +4,43 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 
 function HomeSlider() {
-    const slides = [
-        {
-          url: 'https://unsplash.com/photos/8CqDvPuo_kI/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTJ8fGVkdWNhdGlvbnxlbnwwfHx8fDE3MDI5ODk4ODF8MA&force=true&w=1920',
-        },
-        {
-          url: 'https://unsplash.com/photos/S3nUOqDmUvc/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzAzMDAxMjgwfA&force=true&w=1920',
-        },
-        {
-          url: 'https://unsplash.com/photos/FHnnjk1Yj7Y/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MzB8fGVkdWNhdGlvbnxlbnwwfHx8fDE3MDI5OTY4MTh8MA&force=true&w=1920',
-        },
+    // const slides = [
+    //     {
+    //       url: 'https://unsplash.com/photos/8CqDvPuo_kI/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTJ8fGVkdWNhdGlvbnxlbnwwfHx8fDE3MDI5ODk4ODF8MA&force=true&w=1920',
+    //     },
+    //     {
+    //       url: 'https://unsplash.com/photos/S3nUOqDmUvc/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzAzMDAxMjgwfA&force=true&w=1920',
+    //     },
+    //     {
+    //       url: 'https://unsplash.com/photos/FHnnjk1Yj7Y/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MzB8fGVkdWNhdGlvbnxlbnwwfHx8fDE3MDI5OTY4MTh8MA&force=true&w=1920',
+    //     },
     
-        {
-          url: 'https://unsplash.com/photos/LUGuCtvlk1Q/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8OHx8Ym9va3N8ZW58MHx8fHwxNzAyOTkzODE0fDA&force=true&w=1920',
-        },
-        {
-          url: 'https://unsplash.com/photos/sfL_QOnmy00/download?force=true&w=1920',
-        },
-      ];
+    //     {
+    //       url: 'https://unsplash.com/photos/LUGuCtvlk1Q/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8OHx8Ym9va3N8ZW58MHx8fHwxNzAyOTkzODE0fDA&force=true&w=1920',
+    //     },
+    //     {
+    //       url: 'https://unsplash.com/photos/sfL_QOnmy00/download?force=true&w=1920',
+    //     },
+    //   ];
+
+    const slides = [
+      {
+        url: 'https://ik.imagekit.io/t8uzoinytc/vasily-koloda-8CqDvPuo_kI-unsplash.jpg?updatedAt=1712929284174',
+      },
+      {
+        url: 'https://ik.imagekit.io/t8uzoinytc/elisa-calvet-b-S3nUOqDmUvc-unsplash.jpg?updatedAt=1712929525260',
+      },
+      {
+        url: 'https://ik.imagekit.io/t8uzoinytc/nick-morrison-FHnnjk1Yj7Y-unsplash.jpg?updatedAt=1712929565414',
+      },
+  
+      {
+        url: 'https://ik.imagekit.io/t8uzoinytc/gulfer-ergin-LUGuCtvlk1Q-unsplash.jpg?updatedAt=1712929640685',
+      },
+      {
+        url: 'https://ik.imagekit.io/t8uzoinytc/janko-ferlic-sfL_QOnmy00-unsplash.jpg?updatedAt=1712929669019',
+      },
+    ];
     
       const [currentIndex, setCurrentIndex] = useState(0);
     
